a75f54f12febd488290adb17efadd832d157746b
MSlater <megan.e.slater@gmail.com>
2017-05-23 15:41:24 -0700

    fix cors

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 
-CORS(app)
+CORS(app, resources=r'/api/*')
 
 from models import User
 
